4c7cf4efd53d498d2fc147cc8201df0d3ea74a78
sboardwell <sboardwell@cloudbees.com>
2026-07-22 15:36:25 +0200

    Stop Gerrit server connection in tearDown method

--- src/test/java/com/sonyericsson/hudson/plugins/gerrit/trigger/spec/SpecGerritTriggerHudsonTest.java
+++ src/test/java/com/sonyericsson/hudson/plugins/gerrit/trigger/spec/SpecGerritTriggerHudsonTest.java
@@ -173,6 +173,7 @@ public class SpecGerritTriggerHudsonTest {
      */
     @After
     public void tearDown() throws Exception {
+        gerritServer.stopConnection();
         serverMock.stopServer(sshd);
         sshd = null;
         HazelcastTestHelper.clearAllMaps();
